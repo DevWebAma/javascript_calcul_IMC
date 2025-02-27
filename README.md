639d5ab0f3fb017b6ebb93fd0ff135ed7173a942
DevWebAma <148153050+DevWebAma@users.noreply.github.com>
2025-02-27 02:52:30 +0100

    initial commit (HTML,CSS,JAVASCRIPT)

---
+++ README.md
@@ -0,0 +1,1 @@
+# javascript_calcul_IMC
---
+++ app.js
@@ -0,0 +1,10 @@
+const BMIData = [
+  { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
+  { name: "Bonne santé", color: "green", range: [18.5, 25] },
+  { name: "Surpoids", color: "lightcoral", range: [25, 30] },
+  { name: "obésité modérée", color: "orange", range: [30, 35] },
+  { name: "obésité sévère", color: "crimson", range: [35, 40] },
+  { name: "obésité morbide", color: "purple", range: [40] },
+];
+
+//  IMC = poids en kg/taille (au carré) en m
